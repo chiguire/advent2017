2a628dad4b7d33507f77c72aa55f0585844171f7
Ciro Duran <ciro.duran@bossalien.com>
2017-12-05 14:05:09 +0000

    Advent3_1 in progress, determines which side of the square is the number in

--- src/Advent3.hs
+++ src/Advent3.hs
@@ -2,12 +2,29 @@ module Advent3
     ( advent3_1, advent3_2
     ) where
 
+data SquareSide = Dn | Lf | Up | Rt deriving (Show)
+
+squaredOdds = [(odd i, (odd i) ^ 2) | i <- [0..]] where odd n = 2 * n + 1
+
+hiBound n = head $ dropWhile (\x -> n > (snd x)) squaredOdds
+
+whichSide n (biggestOdd, biggestOddSquared)
+  | (biggestOddSquared - biggestOdd) < n                        = Dn
+  | (biggestOddSquared - biggestOdd - (biggestOdd - 1)) < n     = Lf
+  | (biggestOddSquared - biggestOdd - (biggestOdd - 1) * 2) < n = Up
+  | (biggestOddSquared - biggestOdd - (biggestOdd - 1) * 3) < n = Rt
+
+-- coordinates (biggestOdd, biggestOddSquare) side
+--  | Dn = 
+
 -- Answers
 
-advent3_1 = 0
+advent3_1 = ws --  coordinates hb ws
+  where hb = hiBound input
+        ws = whichSide input hb
 
 advent3_2 = 0
 
 -- Input
 
-input = 0
+input = 277678 
