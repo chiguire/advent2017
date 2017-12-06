90da3198064c0ff509ffc2c6df504d48a39d1215
Ciro Duran <ciro.duran@bossalien.com>
2017-12-06 13:50:29 +0000

    Advent3_1

--- src/Advent3.hs
+++ src/Advent3.hs
@@ -2,26 +2,36 @@ module Advent3
     ( advent3_1, advent3_2
     ) where
 
-data SquareSide = Dn | Lf | Up | Rt deriving (Show)
+data SquareSide = Dn | Lf | Up | Rt deriving (Show, Eq)
 
-squaredOdds = [(odd i, (odd i) ^ 2) | i <- [0..]] where odd n = 2 * n + 1
+squaredOdds = [(i, odd i, (odd i) ^ 2) | i <- [0..]] where odd n = 2 * n + 1
 
-hiBound n = head $ dropWhile (\x -> n > (snd x)) squaredOdds
+hiBound n = head $ dropWhile (\(_, _, x) -> n > x) squaredOdds
 
-whichSide n (biggestOdd, biggestOddSquared)
+whichSide n (i, biggestOdd, biggestOddSquared)
   | (biggestOddSquared - biggestOdd) < n                        = Dn
   | (biggestOddSquared - biggestOdd - (biggestOdd - 1)) < n     = Lf
   | (biggestOddSquared - biggestOdd - (biggestOdd - 1) * 2) < n = Up
   | (biggestOddSquared - biggestOdd - (biggestOdd - 1) * 3) < n = Rt
+  | otherwise = error $ "ERROR: n: " ++ (show n) ++ " sq: " ++ (show (i, biggestOdd, biggestOddSquared))
 
--- coordinates (biggestOdd, biggestOddSquare) side
---  | Dn = 
+coordinates n (numSeq, biggestOdd, biggestOddSquared) side
+  | side == Dn = (n - middleDn, numSeq)
+  | side == Lf = (- numSeq,     n - middleLf)
+  | side == Up = (middleUp - n, - numSeq)
+  | side == Rt = (numSeq,       middleRt - n)
+  where middleDn = biggestOddSquared - (biggestOdd - 1) * 0 - (biggestOdd `div` 2)
+        middleLf = biggestOddSquared - (biggestOdd - 1) * 1 - (biggestOdd `div` 2)
+        middleUp = biggestOddSquared - (biggestOdd - 1) * 2 - (biggestOdd `div` 2)
+        middleRt = biggestOddSquared - (biggestOdd - 1) * 3 - (biggestOdd `div` 2)
 
 -- Answers
 
-advent3_1 = ws --  coordinates hb ws
+advent3_1 = crsum cr
   where hb = hiBound input
         ws = whichSide input hb
+        cr = coordinates input hb ws
+        crsum (x, y) = (abs x) + (abs y)
 
 advent3_2 = 0
 
